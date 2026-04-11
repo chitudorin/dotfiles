e51fef431279a72a238bd67bc8cfe35245bde95a
Chitu Dorin <dorin@nirods-MacBook-Air.local>
2026-04-11 11:37:24 +0300

    Add dock settings

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -9,6 +9,17 @@ defaults write -g AppleInterfaceStyleSwitchesAutomatically -bool true
 defaults write NSGlobalDomain com.apple.mouse.linear -bool "true"
 defaults write -g com.apple.mouse.scaling -float 6.0
 
+# Auto-hide the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Don't show recent applications in the Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Enable magnification
+defaults write com.apple.dock magnification -bool true
+
+# Set magnification size
+defaults write com.apple.dock largesize -int 100
 ###############################################################################
 # Night Shift (via nightlight CLI)                                            #
 ###############################################################################
@@ -19,3 +30,14 @@ if command -v nightlight &> /dev/null; then
 else
   echo "nightlight is not installed."
 fi
+
+
+###############################################################################
+# dockutil                                                                    #
+###############################################################################
+
+# Remove everything from the Dock
+dockutil --remove all --no-restart
+
+killall Dock
+
