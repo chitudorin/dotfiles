6053a3badaab3fc0fe3bdb4c6479991723e533f8
Chitu Dorin <dorin@nirods-MacBook-Air.local>
2026-04-13 01:06:22 +0300

    Remove x86 brew setup

--- setup_brew.sh
+++ setup_brew.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 NON_ADMIN_USER=$(whoami)
-
-if [[ $(uname -m) == 'arm64'  ]]; then
-    CHOWN_CMD="sudo chown -R $NON_ADMIN_USER:staff /opt/homebrew"
-    BREW_BIN="/opt/homebrew/bin/brew"
-else
-    CHOWN_CMD="sudo chown -R $NON_ADMIN_USER:staff /usr/local/Homebrew /usr/local/Caskroom /usr/local/Cellar /usr/local/var/Homebrew"
-    BREW_BIN="/usr/local/bin/brew"
-fi
-
+CHOWN_CMD="sudo chown -R $NON_ADMIN_USER:staff /opt/homebrew"
+BREW_BIN="/opt/homebrew/bin/brew"
 ADMIN_CMD="NONINTERACTIVE=1 /bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" && $CHOWN_CMD"
 
 read -p "Enter your admin username: " ADMIN_USER
@@ -18,10 +11,3 @@ echo "You'll be prompted to enter the admin password several times."
 
 su "$ADMIN_USER" -c "sudo -v && $ADMIN_CMD"
 
-if [ $? -eq 0 ]; then
-    echo "Success! Run these to add brew to PATH:"
-    echo "echo 'eval \"\$($BREW_BIN shellenv)\"' >> /Users/$NON_ADMIN_USER/.zprofile"
-    echo "eval \"\$($BREW_BIN shellenv)\""
-else
-    echo "Something went wrong!"
-fi
